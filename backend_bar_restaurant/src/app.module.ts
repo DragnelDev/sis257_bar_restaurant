9c8050afb6fb758023c5a09cec1720f519eba775
Jhonatan Copa <jhonatanjhoel742@gmail.com>
2025-10-12 17:33:47 -0300

    feat: crear entidades ...

--- backend_bar_restaurant/src/app.module.ts
+++ backend_bar_restaurant/src/app.module.ts
@@ -3,12 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< HEAD
+
 import { ProductosModule } from './productos/productos.module';
 import { CategoriasModule } from './categorias/categorias.module';
 import { ProveedoresModule } from './proveedores/proveedores.module';
-=======
->>>>>>> 04ce6a9faece93539df60e77f84dd41fc49d77e4
 
 @Module({
   imports: [
@@ -22,15 +20,10 @@ import { ProveedoresModule } from './proveedores/proveedores.module';
       database: process.env.DB_NAME,
       entities: [__dirname + '*/**/entities/*.{ts|js}'],
       synchronize: true,
-<<<<<<< HEAD
     }),
     ProductosModule,
     CategoriasModule,
     ProveedoresModule,
-=======
-      autoLoadEntities: true,
-    }),
->>>>>>> 04ce6a9faece93539df60e77f84dd41fc49d77e4
   ],
   controllers: [AppController],
   providers: [AppService],
