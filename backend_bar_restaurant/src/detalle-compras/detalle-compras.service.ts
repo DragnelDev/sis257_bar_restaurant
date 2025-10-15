4a9218e7e30e4727fac7f568e83e4de31970d9f7
macDev <scandragnel@gmail.com>
2025-10-14 23:41:25 -0400

    feat: Implementar CRUD basico detalle comprar

--- backend_bar_restaurant/src/detalle-compras/detalle-compras.service.ts
+++ backend_bar_restaurant/src/detalle-compras/detalle-compras.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateDetalleCompraDto } from './dto/create-detalle-compra.dto';
 import { UpdateDetalleCompraDto } from './dto/update-detalle-compra.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DetalleCompra } from './entities/detalle-compra.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DetalleComprasService {
-  create(createDetalleCompraDto: CreateDetalleCompraDto) {
-    return 'This action adds a new detalleCompra';
+  constructor(
+    @InjectRepository(DetalleCompra)
+    private detalleComprasRepository: Repository<DetalleCompra>,
+  ) {}
+
+  async create(
+    createDetalleCompraDto: CreateDetalleCompraDto,
+  ): Promise<DetalleCompra> {
+    let detalleCompra = await this.detalleComprasRepository.findOneBy({
+      idCompra: createDetalleCompraDto.idCompra,
+      idProducto: createDetalleCompraDto.idProducto,
+      cantidad: createDetalleCompraDto.cantidad,
+      precioUnitarioCompra: createDetalleCompraDto.precioUnitarioCompra,
+    });
+    if (detalleCompra)
+      throw new ConflictException('El detalle compra ya existe');
+
+    detalleCompra = new DetalleCompra();
+    Object.assign(detalleCompra, createDetalleCompraDto);
+    return this.detalleComprasRepository.save(detalleCompra);
   }
 
-  findAll() {
-    return `This action returns all detalleCompras`;
+  async findAll(): Promise<DetalleCompra[]> {
+    return this.detalleComprasRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} detalleCompra`;
+  async findOne(id: number): Promise<DetalleCompra> {
+    const detalleCompra = await this.detalleComprasRepository.findOneBy({ id });
+    if (!detalleCompra)
+      throw new NotFoundException('El detalle compra no existe');
+    return detalleCompra;
   }
 
-  update(id: number, updateDetalleCompraDto: UpdateDetalleCompraDto) {
-    return `This action updates a #${id} detalleCompra`;
+  async update(
+    id: number,
+    updateDetalleCompraDto: UpdateDetalleCompraDto,
+  ): Promise<DetalleCompra> {
+    const detalleCompra = await this.findOne(id);
+    Object.assign(detalleCompra, updateDetalleCompraDto);
+    return this.detalleComprasRepository.save(detalleCompra);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} detalleCompra`;
+  async remove(id: number): Promise<DetalleCompra> {
+    const detalleCompra = await this.findOne(id);
+    return this.detalleComprasRepository.softRemove(detalleCompra);
   }
 }
